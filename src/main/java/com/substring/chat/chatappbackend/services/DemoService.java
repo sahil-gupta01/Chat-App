24397045ea8d80fe662b198eeba4c67a05694e7a
Sahil Gupta <myid1294yo@gmail.com>
2024-12-01 21:42:46 +0530

    branch1 change

--- src/main/java/com/substring/chat/chatappbackend/services/DemoService.java
+++ src/main/java/com/substring/chat/chatappbackend/services/DemoService.java
@@ -2,4 +2,5 @@ package com.substring.chat.chatappbackend.services;
 
 public class DemoService {
 // this is combined merge comment to resolve conflicts
+    //further code has been written in the branch1
 }
